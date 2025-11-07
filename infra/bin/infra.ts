48c225a81e816752e1a0f641ff9c51d4e9a6aaf5
Franco Rezabek <rezabekf@amazon.co.uk>
2025-11-07 16:51:42 +0000

    add cdk-nag

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,7 +1,10 @@
 #!/opt/homebrew/opt/node/bin/node
 import * as cdk from 'aws-cdk-lib';
+import { AwsSolutionsChecks } from 'cdk-nag';
 
 import { InfraStack } from '../lib/infra-stack';
 
 const app = new cdk.App();
 new InfraStack(app, 'InfraStack', {});
+
+cdk.Aspects.of(app).add(new AwsSolutionsChecks({ verbose: true }));
