ff0add69d26212e09ffa7bde7a390b2242f39c0c
Franco Rezabek <rezabekf@amazon.co.uk>
2025-11-28 13:12:00 +0000

    fix metrics permission

--- infra/lib/agent.ts
+++ infra/lib/agent.ts
@@ -97,11 +97,6 @@ export class Agent extends Construct {
             effect: Effect.ALLOW,
             actions: ['cloudwatch:GetMetricStatistics', 'cloudwatch:ListMetrics'],
             resources: ['*'],
-            conditions: {
-              StringEquals: {
-                'cloudwatch:namespace': 'AWS/Lambda',
-              },
-            },
           }),
           new PolicyStatement({
             sid: 'CloudWatchLogsQueryAccess',
